f625e57396387a4653c9e195d30bc3631441bcfd
Błażej Huminiecki <blahy998@gmail.com>
2019-11-23 17:02:23 +0100

    Initiated repo

---
+++ main.py
@@ -0,0 +1,55 @@
+import tensorflow as ts
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+import re
+from config import IMG_HEIGHT, IMG_WIDTH, TRAIN_DIR, TRAIN_SIZE, TEST_DIR, TEST_SIZE
+
+
+def generate_data(image):
+    orb = cv.ORB_create()
+    kp = orb.detect(image, None)
+    kp, des = orb.compute(image, kp)
+    return des
+
+
+train_flags = []
+train_labels = []
+test_flags = []
+test_labels = []
+
+train_image_generator = ImageDataGenerator(rescale=1./255)
+validation_image_generator = ImageDataGenerator(rescale=1./255)
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=TRAIN_SIZE,
+                                                           directory=TRAIN_DIR,
+                                                           shuffle=True,
+                                                           target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                           class_mode='categorical')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=TEST_SIZE,
+                                                              directory=TEST_DIR,
+                                                              target_size=(IMG_HEIGHT, IMG_WIDTH),
+                                                              class_mode='binary')
+
+model = keras.Sequential([
+    keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH ,3)),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+    keras.layers.MaxPooling2D(),
+    keras.layers.Flatten(input_shape=(1, 32, 32, 3)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(28, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_data_gen, validation_data=val_data_gen, epochs=5)
+
+#test_loss, test_acc = model.evaluate(test_flags, test_labels)
+
+#print(test_acc)
